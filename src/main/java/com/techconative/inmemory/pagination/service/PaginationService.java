791f66b27eb0d5316eaab043e4f4fc2f28d9d81f
bharathyes <bharathsoftdev@gmail.com>
2022-08-29 11:01:39 +0530

    Flat filtering
    * adds single layer filtering
    * add cucumber dependencies

--- src/main/java/com/techconative/inmemory/pagination/service/PaginationService.java
+++ src/main/java/com/techconative/inmemory/pagination/service/PaginationService.java
@@ -11,35 +11,37 @@ import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 
 public abstract class PaginationService<T> implements IPaginationService {
 
     private static final ObjectMapper objectMapper = new ObjectMapper();
+
     @Override
     public PageResult getPageResult(PaginationCriteria criteria) {
 
         PageResult pageResult = new PageResult();
 
-        List<Map<String,String>> rawData = convert(getRawData());
+        List<Map<String, String>> rawData = convert(getRawData());
 
 
         //Step 1 : Apply filtering
         //filter=firstName:krishnan~lastname:gopal
         // multiMedia.name = krishnan and lastname = gopal
-        if( criteria.getFilter()!=null && !criteria.getFilter().isEmpty())
-            rawData = applyFiltering(criteria,rawData);
+        if (criteria.getFilter() != null && !criteria.getFilter().isEmpty())
+            rawData = applyFiltering(criteria, rawData);
 
         //Step 2 : Apply search
         // query = krishnan | vishnu
-        if( criteria.getQuery()!=null && !criteria.getQuery().isEmpty())
-            rawData = applySearch(criteria,rawData);
+        if (criteria.getQuery() != null && !criteria.getQuery().isEmpty())
+            rawData = applySearch(criteria, rawData);
 
         //Step 3 : Apply sorting
-        LinkedList<Map<String,String>> sortedList = applySorting(criteria, rawData);
+        LinkedList<Map<String, String>> sortedList = applySorting(criteria, rawData);
 
         //Step 4 : Apply limit and offset
-        LinkedList<Map<String,String>> resultList =  applyPagination(criteria,sortedList);
+        LinkedList<Map<String, String>> resultList = applyPagination(criteria, sortedList);
 
 
         pageResult.setData(resultList);
@@ -52,31 +54,36 @@ public abstract class PaginationService<T> implements IPaginationService {
     }
 
 
-
-    private List<Map<String, String>> convert(List rawData){
-        return rawData.stream().map(item-> {
-            return objectMapper.convertValue(item,Map.class);
-        }).toList();
-    }
-    private List<Map<String, String>> applyFiltering(PaginationCriteria criteria, List<Map<String,String>> rawData) {
-
-        //rawData.stream().anyMatch(map -> map.containsValue(criteria.get))
-        //TODO : filter the values based on the cirteria and return the list.
-        return rawData;
+    private List<Map<String, String>> convert(List rawData) {
+        return rawData.stream().map(item -> objectMapper.convertValue(item, Map.class)).toList();
     }
 
+    private List<Map<String, String>> applyFiltering(PaginationCriteria criteria, List<Map<String, String>> rawData) {
+
+        Map<String, String> filterMap = convertFilterCriteria(criteria.getFilter());
+
+        return rawData.stream()
+                .filter(row ->
+                        filterMap.entrySet().stream().allMatch(e -> e.getValue().equals(row.get(e.getKey()))))
+                .toList();
+    }
+
+    public static Map<String, String> convertFilterCriteria(String filter) {
+        return Stream.of(filter.split("~"))
+                .map(str -> str.split(":"))
+                .collect(Collectors.toMap(str -> str[0], str -> str[1]));
+    }
 
     private LinkedList<Map<String, String>> applySorting(PaginationCriteria criteria, List<Map<String, String>> filteredList) {
         LinkedList<Map<String, String>> sortedList = null;
-        if(criteria.getSort().equals(OrderingCriteria.DESC)){
+        if (criteria.getSort().equals(OrderingCriteria.DESC)) {
             sortedList = filteredList.stream()
                     .sorted(Comparator.comparing(m -> String.valueOf(m.get(criteria.getColumn())),
-                                                 Comparator.nullsFirst(Comparator.reverseOrder()))).collect(
+                            Comparator.nullsFirst(Comparator.reverseOrder()))).collect(
                             Collectors.toCollection(LinkedList::new));
-        }
-        else{
-            sortedList = filteredList.stream().sorted(Comparator.comparing(m ->String.valueOf(m.get(criteria.getColumn())),
-                                                              Comparator.nullsFirst(Comparator.naturalOrder()))).collect(
+        } else {
+            sortedList = filteredList.stream().sorted(Comparator.comparing(m -> String.valueOf(m.get(criteria.getColumn())),
+                    Comparator.nullsFirst(Comparator.naturalOrder()))).collect(
                     Collectors.toCollection(LinkedList::new));
         }
         return sortedList;
@@ -89,13 +96,12 @@ public abstract class PaginationService<T> implements IPaginationService {
     }
 
     private LinkedList applyPagination(PaginationCriteria criteria, LinkedList sortedList) {
-        if(criteria.getLimit() <= 0){
+        if (criteria.getLimit() <= 0) {
             return sortedList;
         }
         int skipCount = (criteria.getPageNumber() - 1) * criteria.getLimit();
         return (LinkedList) sortedList.stream().skip(skipCount).limit(criteria.getLimit()).collect(Collectors.toCollection(LinkedList::new));
     }
-
 
 
     protected abstract List<T> getRawData();
--- src/test/java/com/techconative/inmemory/pagination/FeedService.java
+++ src/test/java/com/techconative/inmemory/pagination/FeedService.java
@@ -16,25 +16,27 @@ import java.util.List;
 public class FeedService extends PaginationService<Feed> {
 
     private static ObjectMapper mapper = new ObjectMapper();
+
     @Override
     protected List<Feed> getRawData() {
 
         try {
-            return mapper.readValue(this.getClass().getClassLoader().getResourceAsStream("Feeds_with_500_Records.json"),
-                                    new TypeReference<List<Feed>>() {});
-        }
-        catch (IOException e) {
+            return mapper.readValue(this.getClass().getClassLoader()
+                            .getResourceAsStream("Feeds_with_500_Records.json"),
+                    new TypeReference<List<Feed>>() {
+                    });
+        } catch (IOException e) {
             throw new RuntimeException(e);
         }
     }
-
-
 
 
     public static void main(String[] args) {
         IPaginationService t = new FeedService();
 
         PaginationCriteria criteria = new PaginationCriteria();
+        criteria.setFilter("title:QQQ~name:bharath");
+//        criteria.setFilter("Gender:Male~State:Ohio");
         criteria.setLimit(10);
         criteria.setColumn("id");
         criteria.setSort(OrderingCriteria.ASC);
